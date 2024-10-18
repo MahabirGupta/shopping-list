ab721b0b2c1e71298be27962ac8c861efd58ef13
MahabirGupta <mahabirg81@gmail.com>
2024-10-18 21:25:03 +0800

    Create the header and footer and style them in the index.css file

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,5 +1,15 @@
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Main from "./component/Main";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="appWrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ shopping-list/src/component/Footer.jsx
@@ -0,0 +1,4 @@
+const Footer = () => {
+  return <div className="footerWrapper">Total Item:X Completed Item:X</div>;
+};
+export default Footer;
